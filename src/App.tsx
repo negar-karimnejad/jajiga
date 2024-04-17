499ec452a8cd15b41db56db62c97b50ff7a3f632
negar <negar.karimnejad@gmail.com>
2024-04-17 11:47:34 +0330

    update title and site icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="dark:bg-white bg-red-500 text-white">
-      <h1 className="text-xl text-black dark:bg-stone-600 hover:animate-ping">Hi world</h1>
+      <h1 className="text-xl text-black dark:bg-stone-600 hover:text-rose-500">
+        Hi world
+      </h1>
     </div>
   );
 }
