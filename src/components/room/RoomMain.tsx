b4e470e9352f193ada3dcff151801942c1f7e7c3
negar <negar.karimnejad@gmail.com>
2024-05-01 21:27:05 +0330

    coding

--- src/components/room/RoomMain.tsx
+++ src/components/room/RoomMain.tsx
@@ -1,111 +1,117 @@
-import { useState } from 'react';
-import { GoQuestion, GoShieldCheck } from 'react-icons/go';
-import { HiOutlineInformationCircle } from 'react-icons/hi';
-import { HiOutlineChatBubbleLeftRight } from 'react-icons/hi2';
+import { BsStar, BsStarFill } from 'react-icons/bs';
+import { HiOutlineHome } from 'react-icons/hi';
+import { LiaDoorOpenSolid } from 'react-icons/lia';
+import { PiUsersThree } from 'react-icons/pi';
+import { TfiRulerPencil } from 'react-icons/tfi';
+import Breadcrumb from '../ui/Breadcrumb';
 import Button from '../ui/Button';
+import RoomSidebar from './RoomSidebar';
 
 function RoomMain() {
-  const [isShowInfo, setIsShowInfo] = useState(false);
   return (
-    <div className="container my-10 flex h-[1000rem] flex-col gap-10 md:grid md:grid-cols-12">
-      <div className="bg-green-500 md:col-span-8">g</div>
-      <div className="sticky top-20 h-fit md:col-span-4">
-        <div className="overflow-hidden rounded-xl shadow-lg">
-          <header className="flex justify-between bg-neutral-700 p-4 text-white">
-            <span className="font-vazirMedium text-base">نرخ هر شب از:</span>
-            <span className="font-persianNums text-base">
-              {(2800000).toLocaleString()} تومان
-            </span>
-          </header>
-          <div className="my-6 px-4">
-            <form className="flex flex-col gap-5">
-              <label htmlFor="">
-                تاریخ سفر
-                <div className="relative mt-1 flex overflow-hidden rounded-xl border px-4 py-3">
-                  <input
-                    type="text"
-                    className="outline-none"
-                    placeholder="تاریخ ورود"
-                  />
-                  <div className="absolute left-0 right-0 mx-auto h-6 w-0.5 bg-gray-200"></div>
-                  <input
-                    type="text"
-                    className="outline-none"
-                    placeholder="تاریخ خروج"
-                  />
-                </div>
-              </label>
-              <label htmlFor="" className="flex flex-col">
-                تعداد نفرات
-                <select
-                  name=""
-                  id=""
-                  className="mt-1 rounded-xl border px-4 py-2 text-gray-400 outline-none"
-                >
-                  <option value="" className="text-gray-500">
-                    تعداد نفرات را مشخص کنید
-                  </option>
-                  <option value="" className="font-persianNums text-gray-700">
-                    1 نفر
-                  </option>
-                  <option value="" className="font-persianNums text-gray-700">
-                    2 نفر
-                  </option>
-                  <option value="" className="font-persianNums text-gray-700">
-                    3 نفر
-                  </option>
-                  <option value="" className="font-persianNums text-gray-700">
-                    4 نفر
-                  </option>
-                  <option value="" className="font-persianNums text-gray-700">
-                    5 نفر
-                  </option>
-                  <option value="" className="font-persianNums text-gray-700">
-                    6 نفر
-                  </option>
-                </select>
-                <p className="mt-1 text-[12px] text-gray-500">
-                  تا 1 کودک زیر 5 سال در صورتحساب لحاظ نمی گردد.
-                </p>
-              </label>
-              <Button style="font-vazirMedium relative bg-yellow-400 hover:bg-yellow-500 rounded-full w-full my-5">
-                ثبت درخواست رزرو{' '}
-                <span className="font-vazirMedium text-[12px]">(رایگان)</span>
-              </Button>
-            </form>
-            <p className="mb-4 flex items-center justify-center gap-2 text-center text-[13px] text-gray-600">
-              <HiOutlineChatBubbleLeftRight size={18} />
-              با امکان چت آنلاین با میزبان
-              <div
-                className="relative"
-                onMouseEnter={() => setIsShowInfo(true)}
-                onMouseLeave={() => setIsShowInfo(false)}
-              >
-                <HiOutlineInformationCircle
-                  size={18}
-                  className="text-sky-400"
-                />
-                <div
-                  className={`absolute -left-2 -top-20 whitespace-nowrap rounded-lg bg-neutral-700 px-5 py-3 text-sm leading-6 text-white shadow-lg after:absolute after:-bottom-1 after:left-3 after:h-2 after:w-2 after:rotate-45 after:bg-neutral-700 ${isShowInfo ? 'block' : 'hidden'}`}
-                >
-                  برای چت با میزبان ابتدا درخواست رزرو
-                  <br /> (رایگان) خود را ثبت نمایید.
-                </div>
-              </div>
-            </p>
-            <div className="flex flex-col gap-3 lg:flex-row">
-              <Button style="flex items-center gap-2 bg-transparent rounded-xl border-2 border-gray-100 hover:border-gray-200 hover:shadow-none w-full">
-                <GoShieldCheck />
-                ضمانت تحویل
-              </Button>
-              <Button style="flex items-center gap-2 bg-transparent rounded-xl border-2 border-gray-100 hover:border-gray-200 hover:shadow-none w-full">
-                <GoQuestion />
-                راهنمای رزرو
-              </Button>
-            </div>
+    <div className="container my-10 flex flex-col gap-5 md:grid md:grid-cols-12">
+      <div className="md:col-span-8">
+        <Breadcrumb pageLink="ساوجبلاغ" links={['البرز']} />
+        <div className="flex items-center justify-between">
+          <h4 className="font-vazirBold text-base dark:text-white">
+            اجاره ویلا استخردار در ساوجبلاغ - کوشک زر
+          </h4>
+          <div className="h-16 w-16">
+            <img src="/images/comment.jpg" className="rounded-full" alt="" />
           </div>
         </div>
+
+        <div className="mt-10 flex items-center gap-2">
+          <Button style="font-persianNums text-[13px] bg-yellow-500 rounded-full py-1 hover:shadow-none ">
+            کد: 123456
+          </Button>
+          <Button style="cursor-default font-persianNums text-[13px] bg-gray-200 rounded-full py-1 hover:shadow-none ">
+            10+ رزرو موفق
+          </Button>
+          <div className="flex gap-0.5">
+            <BsStar className="text-yellow-400" />
+            <BsStarFill className="text-yellow-400" />
+            <BsStarFill className="text-yellow-400" />
+            <BsStarFill className="text-yellow-400" />
+            <BsStarFill className="text-yellow-400" />
+          </div>
+          <span className="font-persianNums text-[12px] text-gray-500 dark:text-gray-300">
+            4.7
+          </span>
+          <span className="font-persianNums text-[12px] text-gray-500 dark:text-gray-300">
+            (10 نظر)
+          </span>
+        </div>
+
+        <div className="mt-5 flex items-center justify-around bg-gray-100 p-5 dark:bg-gray-800">
+          <div className="flex flex-col items-center gap-2">
+            <HiOutlineHome className="dark:text-gray-200" size={30} />
+            <span className="font-persianNums text-[13px] dark:text-gray-200">
+              دربست
+            </span>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <LiaDoorOpenSolid className="dark:text-gray-200" size={30} />
+            <span className="font-persianNums text-[13px] dark:text-gray-200">
+              2 اتاق خواب
+            </span>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <PiUsersThree className="dark:text-gray-200" size={30} />
+            <span className="font-persianNums text-[13px] dark:text-gray-200">
+              تا 6 مهمان
+            </span>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <TfiRulerPencil className="dark:text-gray-200" size={30} />
+            <span className="font-persianNums text-[13px] dark:text-gray-200">
+              220 متر
+            </span>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h4 className="font-vazirBold text-lg">درباره اقامتگاه</h4>
+          <p className="mt-5 text-sm leading-6 text-gray-600">
+            <strong className="text-gray-900">
+              اجاره ویلا استخردار در ساوجبلاغ - کوشک زر
+            </strong>
+            <br />
+            این ویلا دو خوابه با استخر سرپوشیده آبگرم، روف گاردن، میز و صندلی
+            تاشو، آتشدان، فضای دورهمی و حیاطی دلباز در روستای کوشک زر با فاصله
+            حدود <span className="font-persianNums">20</span> کیلومتری از کردان
+            و <span className="font-persianNums">35</span> کیلومتری از کرج در محیطی شهرکی واقع شده است.
+            <br />
+            طراحی اقامتگاه به گونه ایست که جکوزی، استخر در طبقه منفی یک و
+            پذیرایی، آشپزخانه، دو اتاق خواب، حمام و سرویس ایرانی با فرنگی در
+            طبقه همکف با دسترسی حدود <span className="font-persianNums">11</span> پله و روف گاردن در طبقه بالا با دسترسی
+            حدود <span className="font-persianNums">13</span> پله تعبیه شده است.
+            <br />
+            اطراف محوطه اقامتگاه با دیوار محصور و مجهز به دوربین مداربسته می
+            باشد.
+            <br />
+            برای تهیه مایحتاج روزانه فاصله اقامتگاه تا سوپرمارکت و نانوایی حدود
+            یک کیلومتر است.
+            <br />
+            لازم به ذکر است که آب لوله کشی اقامتگاه از طریق چاه تامین می گردد
+            لذا به مهمانان گرامی توصیه می شود که آب معدنی به همراه داشته باشند.
+            <br />
+            کیفیت پوشش شبکه تلفن همراه برای دو اپراتور همراه اول و ایرانسل در
+            مکالمه خوب و دسترسی به اینترنت به صورت <span className="font-persianNums">4</span>g می باشد.
+            <br />
+            حدود <span className="font-persianNums">500</span> متر از مسیر منتهی به اقامتگاه به صورت جاده خاکی می باشد.
+            <br />
+            <strong className="text-gray-900">
+              خانه ای در نزدیکی این اقامتگاه درحال ساخت و ساز است.
+            </strong>
+            <br />
+            <strong className="text-gray-900">
+              استخر روباز موجود در محوطه در حال حاضر غیر فعال می باشد.
+            </strong>
+          </p>
+        </div>
       </div>
+      <RoomSidebar />
     </div>
   );
 }
---
+++ src/components/room/RoomSidebar.tsx
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import { GoQuestion, GoShieldCheck } from 'react-icons/go';
+import { HiOutlineInformationCircle } from 'react-icons/hi';
+import { HiOutlineChatBubbleLeftRight } from 'react-icons/hi2';
+import Button from '../ui/Button';
+
+function RoomSidebar() {
+  const [isShowInfo, setIsShowInfo] = useState(false);
+
+  return (
+    <div className="sticky top-20 h-fit md:col-span-4">
+      <div className="overflow-hidden rounded-xl shadow-lg dark:border dark:border-gray-700 dark:bg-gray-800">
+        <header className="flex justify-between bg-neutral-700 p-4 text-white">
+          <span className="font-vazirMedium text-base">نرخ هر شب از:</span>
+          <span className="font-persianNums text-base">
+            {(2800000).toLocaleString()} تومان
+          </span>
+        </header>
+        <div className="my-6 px-4">
+          <form className="flex flex-col gap-5">
+            <label htmlFor="" className="dark:text-white">
+              تاریخ سفر
+              <div className="relative mt-1 flex overflow-hidden rounded-xl border px-4 py-3">
+                <input
+                  type="text"
+                  className="bg-transparent outline-none"
+                  placeholder="تاریخ ورود"
+                />
+                <div className="absolute left-0 right-0 mx-auto h-6 w-0.5 bg-gray-200"></div>
+                <input
+                  type="text"
+                  className="bg-transparent outline-none"
+                  placeholder="تاریخ خروج"
+                />
+              </div>
+            </label>
+            <label htmlFor="" className="flex flex-col dark:text-white">
+              تعداد نفرات
+              <select
+                name=""
+                id=""
+                className="mt-1 rounded-xl border bg-transparent px-4 py-2 text-gray-400 outline-none"
+              >
+                <option value="" className="text-gray-500">
+                  تعداد نفرات را مشخص کنید
+                </option>
+                <option value="1" className="font-persianNums text-gray-700">
+                  1 نفر
+                </option>
+                <option value="2" className="font-persianNums text-gray-700">
+                  2 نفر
+                </option>
+                <option value="3" className="font-persianNums text-gray-700">
+                  3 نفر
+                </option>
+                <option value="4" className="font-persianNums text-gray-700">
+                  4 نفر
+                </option>
+                <option value="5" className="font-persianNums text-gray-700">
+                  5 نفر
+                </option>
+                <option value="6" className="font-persianNums text-gray-700">
+                  6 نفر
+                </option>
+              </select>
+              <p className="mt-1 text-[12px] text-gray-500 dark:text-gray-200">
+                تا 1 کودک زیر 5 سال در صورتحساب لحاظ نمی گردد.
+              </p>
+            </label>
+            <Button style="font-vazirMedium relative bg-yellow-400 hover:bg-yellow-500 rounded-full w-full my-5">
+              ثبت درخواست رزرو{' '}
+              <span className="font-vazirMedium text-[12px]">(رایگان)</span>
+            </Button>
+          </form>
+          <p className="mb-4 flex items-center justify-center gap-2 text-center text-[13px] text-gray-600 dark:text-gray-100">
+            <HiOutlineChatBubbleLeftRight size={18} />
+            با امکان چت آنلاین با میزبان
+            <div
+              className="relative"
+              onMouseEnter={() => setIsShowInfo(true)}
+              onMouseLeave={() => setIsShowInfo(false)}
+            >
+              <HiOutlineInformationCircle size={18} className="text-sky-400" />
+              <div
+                className={`absolute -left-2 -top-20 whitespace-nowrap rounded-lg bg-neutral-700 px-5 py-3 text-sm leading-6 text-white shadow-lg after:absolute after:-bottom-1 after:left-3 after:h-2 after:w-2 after:rotate-45 after:bg-neutral-700 ${isShowInfo ? 'block' : 'hidden'}`}
+              >
+                برای چت با میزبان ابتدا درخواست رزرو
+                <br /> (رایگان) خود را ثبت نمایید.
+              </div>
+            </div>
+          </p>
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <Button style="dark:text-gray-50 dark:border-gray-300 flex items-center gap-2 bg-transparent rounded-xl border-2 border-gray-100 hover:border-gray-200 hover:shadow-none w-full">
+              <GoShieldCheck />
+              ضمانت تحویل
+            </Button>
+            <Button style="dark:text-gray-50 dark:border-gray-300 flex items-center gap-2 bg-transparent rounded-xl border-2 border-gray-100 hover:border-gray-200 hover:shadow-none w-full">
+              <GoQuestion />
+              راهنمای رزرو
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RoomSidebar;
