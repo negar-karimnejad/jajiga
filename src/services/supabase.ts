8a59b1b8faed981f37572d0115dd0747b91685f1
negar <negar.karimnejad@gmail.com>
2024-06-16 16:56:04 +0330

    coding

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,6 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 export const supabaseUrl = import.meta.env.VITE_REACT_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_REACT_SUPABASE_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Missing Supabase URL or key.");
+}
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Key:', supabaseKey);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
