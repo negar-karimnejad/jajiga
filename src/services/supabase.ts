72c38e04bc04bea102a1495509c4f20b71212600
negar <negar.karimnejad@gmail.com>
2024-06-16 14:41:38 +0330

    coding

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabaseUrl = import.meta.env.REACT_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.REACT_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
