1b3e7130a67167529f1ea8915b3dc41e1560ab92
negar <negar.karimnejad@gmail.com>
2024-06-16 14:49:32 +0330

    coding

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = import.meta.env.REACT_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.REACT_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
